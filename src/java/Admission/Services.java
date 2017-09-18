7a05f3df4cb14ed96c0652b97ba6c2622bc6b209
Leeonore <leonore@grandguillotte.fr>
2017-09-18 14:57:24 +0200

    Services

--- src/java/Admission/Services.java
+++ src/java/Admission/Services.java
@@ -5,6 +5,7 @@
  */
 package Admission;
 
+import java.util.Date;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -24,10 +25,11 @@ public class Services {
         this.fact = fact;
         this.em = fact.createEntityManager();
     }
-    
-     public void close() {
+    public void close() {
         em.close();
     }
+    
+/** Gestion des patients **/
     public void newPatient(Patient pat){
         //Créer l'objet patient dans la base
         em.getTransaction( ).begin( );
@@ -51,21 +53,29 @@ public class Services {
         em.getTransaction().commit();
     }
     
-    public void removePat(String IPP) {
-        Patient pat = em.findPat( Patient.class, IPP ); //ça va marcher ???
+    public void removePatient(String IPP) {
+        Patient pat = em.find( Patient.class, IPP ); //ça va marcher ???
 	em.getTransaction( ).begin( );
         em.remove(pat);
         em.getTransaction().commit();
     }
+    
     public void editPatient(Patient pat){
         //Modifier une patient
         em.getTransaction( ).begin( );
-        em.merge(cr);
+        em.merge(pat);
         em.getTransaction().commit();
     }
     
-    public void FindPatient(){
+    public Patient getPatientByIPP(String IPP) {       
+	Patient res = em.find( Patient.class, IPP );      
+        return res;
+    }
+    
+    public void FindPatient(String nom, String prenom, Date dateNaiss ){
         // Rechercher un patient par nom, prénom, date de naiss
+        TypedQuery<Patient> query = em.createQuery("SELECT p FROM Patient p WHERE p.Nom = :nom AND p.prenom=:prenom AND p.dateNaiss = :dateNaiss", Patient.class).setParameter("couleur",couleur); //MODIFIER
+        List<Patient> res = query.getResultList();
     }
     
     public List<Patient> getAllPatient() {
@@ -74,17 +84,43 @@ public class Services {
         return res;
     }
     
-    
+/** Gestion des Venues **/    
     public void newVenue(){
         //Créer une nouvelle venue pour un patient existant (IPP, IEP, DateHeure venue/sortie, UF traitement, type venue)
+        em.getTransaction( ).begin( );
+        em.persist(pat);
+        em.getTransaction().commit();
     }
     
+    public Venue newVenue(String IPP, String IEP, Date dateVenue, Date dateSortie, String UFtraitement, String typeVenue){
+        //Créer une nouvelle venue pour un patient existant (IPP, IEP, DateHeure venue/sortie, UF traitement, type venue)
+        Venue venue= newVenue();
+        venue.setIPP(IPP);
+        venue.setIEP(IEP);
+        venue.setDateVenue(dateVenue);
+        venue.setDateSortie(dateSortie);
+        venue.setUF(UFtraitement);
+        venue.setType(typeVenue);
+        
+        em.getTransaction( ).begin( );
+        em.persist(venue);
+        em.getTransaction().commit();
+    }
+        
     public void FindVenue(){
         // Rechercher une venue
     }
-           
-    public void editVenue(){
+
+    public Venue getVenueByIEP(String IeP) {       
+	Venue res = em.find( Venue.class, IEP );      
+        return res;
+    }
+    
+    public void editVenue(Venue venue){
         //Modifier une venue (dont ajoue date fin)
+        em.getTransaction( ).begin( );
+        em.merge(Venue);
+        em.getTransaction().commit();
     }
     
     public List<Venue> getAllVenue() {
