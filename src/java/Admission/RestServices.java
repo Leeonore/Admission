0efb08fdf3e50778a066c861454d8f378f57a862
Leeonore <leonore@grandguillotte.fr>
2017-11-06 14:07:14 +0100

    TestRest

--- src/java/Admission/RestServices.java
+++ src/java/Admission/RestServices.java
@@ -32,30 +32,34 @@ public class RestServices {
 /* -------------------- RestService sur les patients  */  
     //Liste de tous les patients
     
-    @GET
-    @Path("patients")
-    @Produces("application/json")
-    public List<Patient> getAllPatients(){
-        return serv.getAllPatient();
-    }
-    /*
+//    @GET
+//    @Path("patients")
+//    @Produces("application/json")
+//    public List<Patient> getAllPatients(){
+//        return serv.getAllPatient();
+//    }
+    
     //Recherche de patient par nom, prenom, date de naissance
     @GET
     @Path("patients")
     @Produces("application/json")
     public List<Patient> findPatient(@DefaultValue("") @QueryParam("nom") String nom,@DefaultValue("") @QueryParam("prenom") String prenom,@DefaultValue("") @QueryParam("datenaiss") String dateNaiss){
-        return serv.findPatient(nom, prenom, dateNaiss);
-    }*/
-
-    
-    //Recherche de patient par IPP
-    @GET
-    @Path("patiens/{ipp}")
-    @Produces("application/json")
-    public Patient getPatient(@PathParam("ipp") int IPP) {
-        return serv.getPatientByIPP(IPP);
+        if (nom.equals("") && prenom.equals("") && dateNaiss.equals("")){
+            return serv.getAllPatient();
+        } else {
+            return serv.findPatient(nom, prenom, dateNaiss);
+        }
     }
-    
+//
+//    
+//    //Recherche de patient par IPP
+//    @GET
+//    @Path("patients/{ipp}")
+//    @Produces("application/json")
+//    public Patient getPatient(@PathParam("ipp") int IPP) {
+//        return serv.getPatientByIPP(IPP);
+//    }
+//    
     //Creation d'un patient
     @POST
     @Path("patients")
@@ -65,90 +69,90 @@ public class RestServices {
         serv.newPatient(pat);
         return pat;
     }
-    
-    //Modification d'un patient
-    @POST
-    @Path("patients/{ipp}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response editPatient(Patient pat){
-        serv.editPatient(pat);
-        return Response.status(200).entity(pat).build();
-    }
-    
-    //Suppression de tous les patients
-    @DELETE
-    @Path("/patients")
-    public Response removeAllPatient(){
-        serv.removeAllPatient();
-        return Response.status(200).build();
-    }
-    
-    //Suppression d'un patient
-    @DELETE
-    @Path("/patients/{ipp}")
-    public Response removePatient(@PathParam("ipp") int IPP){
-        serv.removePatient(IPP);
-        return Response.status(200).build();
-    }
-    
-    
-/* -------------------- RestService sur les venues  */  
-    //Liste de toutes les venues
-    @GET
-    @Path("venues")
-    @Produces("application/json")
-    public List<Venue> getAllVenues(){
-        return serv.getAllVenue();
-    }
-        
-    //Recherche de venue par IPP, IEP, date de venue
-    @GET
-    @Path("patiens?ipp=<IPP>&iep=<IEP>&datevenue=<dateVenue>")
-    @Produces("application/json")
-    public List<Venue> findVenue(@DefaultValue("") @QueryParam("ipp") int IPP,@DefaultValue("") @QueryParam("iep") int IEP,@DefaultValue("") @QueryParam("datevenue") String dateVenue){
-        return serv.findVenue(IPP, IEP, dateVenue);
-    }
-    
-    //Recherche de venue par IEP
-    @GET
-    @Path("venues/{iep}")
-    @Produces("application/json")
-    public Venue getVenue(@PathParam("iep") int IEP) {
-        return serv.getVenueByIEP(IEP);
-    }
-    
-    //Creation d'une venue
-    @POST
-    @Path("venues")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces("application/json")
-    public Venue newVenue(Venue ven){
-        serv.newVenue(ven);
-        return ven;
-    }
-    
-    //Modification d'un patient
-    @POST
-    @Path("venues/{ipp}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response editVenue(Venue ven){
-        serv.editVenue(ven);
-        return Response.status(200).entity(ven).build();
-    }
-    
-    //Suppression de tous les patients
-    @DELETE
-    @Path("/venues")
-    public Response removeAllVenues(){
-        serv.removeAllVenue();
-        return Response.status(200).build();
-    }
-    
-    //Suppression d'un patient
-    @DELETE
-    @Path("/venues/{iep}")
-    public Response removeVenue(@PathParam("iep") int IEP){
-        serv.removeVenue(IEP);
-        return Response.status(200).build();
-    }
+//    
+//    //Modification d'un patient
+//    @POST
+//    @Path("patients/{ipp}")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public Response editPatient(Patient pat){
+//        serv.editPatient(pat);
+//        return Response.status(200).entity(pat).build();
+//    }
+//    
+//    //Suppression de tous les patients
+//    @DELETE
+//    @Path("/patients")
+//    public Response removeAllPatient(){
+//        serv.removeAllPatient();
+//        return Response.status(200).build();
+//    }
+//    
+//    //Suppression d'un patient
+//    @DELETE
+//    @Path("/patients/{ipp}")
+//    public Response removePatient(@PathParam("ipp") int IPP){
+//        serv.removePatient(IPP);
+//        return Response.status(200).build();
+//    }
+//    
+//    
+///* -------------------- RestService sur les venues  */  
+//    //Liste de toutes les venues
+//    @GET
+//    @Path("venues")
+//    @Produces("application/json")
+//    public List<Venue> getAllVenues(){
+//        return serv.getAllVenue();
+//    }
+//        
+//    //Recherche de venue par IPP, IEP, date de venue
+//    @GET
+//    @Path("patients?ipp=<IPP>&iep=<IEP>&datevenue=<dateVenue>")
+//    @Produces("application/json")
+//    public List<Venue> findVenue(@DefaultValue("") @QueryParam("ipp") int IPP,@DefaultValue("") @QueryParam("iep") int IEP,@DefaultValue("") @QueryParam("datevenue") String dateVenue){
+//        return serv.findVenue(IPP, IEP, dateVenue);
+//    }
+//    
+//    //Recherche de venue par IEP
+//    @GET
+//    @Path("venues/{iep}")
+//    @Produces("application/json")
+//    public Venue getVenue(@PathParam("iep") int IEP) {
+//        return serv.getVenueByIEP(IEP);
+//    }
+//    
+//    //Creation d'une venue
+//    @POST
+//    @Path("venues")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    @Produces("application/json")
+//    public Venue newVenue(Venue ven){
+//        serv.newVenue(ven);
+//        return ven;
+//    }
+//    
+//    //Modification d'un patient
+//    @POST
+//    @Path("venues/{ipp}")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public Response editVenue(Venue ven){
+//        serv.editVenue(ven);
+//        return Response.status(200).entity(ven).build();
+//    }
+//    
+//    //Suppression de tous les patients
+//    @DELETE
+//    @Path("/venues")
+//    public Response removeAllVenues(){
+//        serv.removeAllVenue();
+//        return Response.status(200).build();
+//    }
+//    
+//    //Suppression d'un patient
+//    @DELETE
+//    @Path("/venues/{iep}")
+//    public Response removeVenue(@PathParam("iep") int IEP){
+//        serv.removeVenue(IEP);
+//        return Response.status(200).build();
+//    }
 }
